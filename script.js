938e9a67a1e147d963b8be5b0db3765e1ca1dc38
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-08-01 14:54:58 +0100

    script.js file added with array for test questions

---
+++ script.js
@@ -0,0 +1,6 @@
+const questions = [["what color are bananas", "yellow"],
+["what color are green apples", "green"],
+["what color are strawberrys", "red"],
+["what color are oranges", "orange"],
+["what color are tomatos", "red"]];
+
